205b4babb809116a3e00f6705c92f4ade15178bf
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-11-18 23:40:30 -0500

    Fix format flow script

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x
-${CLANG_FORMAT:-clang-format} -i relay_board/*.c relay_board/*.h
+${CLANG_FORMAT:-clang-format} -i --style=file:third_party/rocketlib/.clang-format relay_board/*.c relay_board/*.h
