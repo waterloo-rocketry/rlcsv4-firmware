a8464c117cdfe1cd3c0221b89e1406c7280b641f
Skyler Ma <skyler@skylerma.com>
2025-11-18 23:20:48 -0500

    Init, read, write function declarations and comments for MCP23008 driver

--- relay_board/mcp23008.c
+++ relay_board/mcp23008.c
@@ -1,3 +1,16 @@
+#include <stdint.h>
+
+#include "common.h"
 #include "mcp23008.h"
 
-void mcp23008_init(void) {}
+w_status_t mcp23008_init(uint8_t io_dir) {
+	return W_SUCCESS;
+}
+
+w_status_t mcp23008_read_gpio(uint8_t *data) {
+	return W_SUCCESS;
+}
+
+w_status_t mcp23008_write_gpio(uint8_t data) {
+	return W_SUCCESS;
+}
--- relay_board/mcp23008.h
+++ relay_board/mcp23008.h
@@ -1,6 +1,54 @@
+/**
+ * @file
+ * @brief MCP23008 driver
+ *
+ * This module provides functionality for the MCP23008 I/O expander using the
+ * I2C interface. It supports initialization and reading/writing to its 8 GPIO
+ * pins.
+ */
+
 #ifndef MCP23008_H
 #define MCP23008_H
 
-void mcp23008_init(void);
+#include <stdint.h>
+
+#include "common.h"
+
+/**
+ * @brief Initialize MCP23008 with I/O directions.
+ *
+ * This function initializes the MCP23008 with specified I/O directions.
+ *
+ * Each bit of io_dir determines I/O direction for one pin, with bit number
+ * corresponding to pin number. Bit value of 1 means output, 0 means input. Note
+ * GP7 must be configured as an output.
+ *
+ * @param io_dir I/O directions for each pin
+ * @return w_status_t Returns status code
+ */
+w_status_t mcp23008_init(uint8_t io_dir);
+
+/**
+ * @brief Read all GPIO pins of MCP23008.
+ *
+ * This function reads the values of all GPIO pins. The value of bit X of *data
+ * is set to the read value of pin GPX.
+ *
+ * @param data Pointer to place in which to read GPIO pin values.
+ * @return w_status_t Returns status code
+ */
+w_status_t mcp23008_read_gpio(uint8_t *data);
+
+/**
+ * @brief Writes all GPIO pins configured as outputs of MCP23008.
+ *
+ * This function writes to all GPIO pins which are configured as outputs. The
+ * value of pin GPX will be set to the value of bit X of data (if pin GPX was
+ * configured as an output).
+ *
+ * @param data Binary value representing values to write to pins.
+ * @return w_status_t Returns status code
+ */
+w_status_t mcp23008_write_gpio(uint8_t data);
 
 #endif
