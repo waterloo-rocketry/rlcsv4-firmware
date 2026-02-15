541fdba81dc2aa94f29e7e9643c3cb0d443c1fc8
E. L. Raby <eleoraby@gmail.com>
2026-02-15 13:48:57 -0500

    fix: Added seprate functions for each current_sense channel

--- relay_board/utils/volt_convert.c
+++ relay_board/utils/volt_convert.c
@@ -3,7 +3,7 @@
 #include "adc.h"
 #include "adc_types.h"
 
-float get_adc_voltage() {
+float get_adc_voltage_5v() {
 	adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANB0);
 	float scaled_value =
 		((float)adc_value / 4095.0) * 5.0; // Scale the ADC value then convert to voltage
@@ -11,12 +11,10 @@ float get_adc_voltage() {
 	return scaled_value;
 }
 
-float get_input_voltage() {
-	float voltage = get_adc_voltage() / 100.0;
-	return voltage;
-}
+float get_adc_voltage_12v() {
+    adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANB1);
+	float scaled_value =
+		((float)adc_value / 4095.0) * 12.0; // Scale the ADC value then convert to voltage
 
-float get_input_current() {
-	float voltage = get_input_voltage() / 0.25;
-	return voltage;
+	return scaled_value;
 }
