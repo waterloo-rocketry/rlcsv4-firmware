f0a74cb48a660dec35a8e373dbbebd32a55ff835
E. L. Raby <eleoraby@gmail.com>
2026-02-15 14:16:04 -0500

    fix: Added other channels

--- relay_board/utils/volt_convert.c
+++ relay_board/utils/volt_convert.c
@@ -3,8 +3,17 @@
 #include "adc.h"
 #include "adc_types.h"
 
-float get_adc_voltage_5v() {
-	adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANB0);
+
+float get_adc_voltage_output1() {
+	adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANA5);
+	float scaled_value =
+		((float)adc_value / 4095.0) * 5.0; // Scale the ADC value then convert to voltage
+
+	return scaled_value;
+}
+
+float get_adc_voltage_output2() {
+	adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANC6);
 	float scaled_value =
 		((float)adc_value / 4095.0) * 5.0; // Scale the ADC value then convert to voltage
 
@@ -12,9 +21,19 @@ float get_adc_voltage_5v() {
 }
 
 float get_adc_voltage_12v() {
-    adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANB1);
+    //TODO`: Vdiv
+    adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANC2);
+
 	float scaled_value =
-		((float)adc_value / 4095.0) * 12.0; // Scale the ADC value then convert to voltage
+		((float)adc_value / 4095.0) * 4; // Scale the ADC value then convert to voltage
+
+	return scaled_value;
+}
+
+float get_adc_voltage_battery() {
+	adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANB2);
+	float scaled_value =
+		((float)adc_value / 4095.0) * 5.0; // Scale the ADC value then convert to voltage
 
 	return scaled_value;
 }
