09246ec0d5bef2a1f36445103181449b253601e0
E. L. Raby <eleoraby@gmail.com>
2026-02-15 15:15:02 -0500

    chore: comments, formatting

--- relay_board/utils/volt_convert.c
+++ relay_board/utils/volt_convert.c
@@ -3,37 +3,37 @@
 #include "adc.h"
 #include "adc_types.h"
 
-
 float get_adc_voltage_output1() {
 	adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANA5);
-	float scaled_value =
-		((float)adc_value / 4095.0) * 6.6; // Scale the ADC value then convert to voltage
+
+	// Scale the ADC value then convert to voltage
+	float scaled_value = ((float)adc_value / 4095.0) * 6.6;
 
 	return scaled_value;
 }
 
 float get_adc_voltage_output2() {
 	adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANC6);
-	float scaled_value =
-		((float)adc_value / 4095.0) * 6.6; // Scale the ADC value then convert to voltage
+
+	// Scale the ADC value then convert to voltage (6.6 = (56k + 10k)/10k)
+	float scaled_value = ((float)adc_value / 4095.0) * 6.6;
 
 	return scaled_value;
 }
 
 float get_adc_voltage_12v() {
-    //TODO`: Vdiv
-    adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANC2);
+	adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANC2);
 
-	float scaled_value =
-		((float)adc_value / 4095.0) * 3; // Scale the ADC value then convert to voltage
+	// Scale the ADC value then convert to voltage (3 = (20k + 10k)/10k)
+	float scaled_value = ((float)adc_value / 4095.0) * 3;
 
 	return scaled_value;
 }
 
 float get_adc_voltage_battery() {
 	adc_result_t adc_value = ADC_ChannelSelectAndConvert(ADC_CHANNEL_ANB2);
-	float scaled_value =
-		((float)adc_value / 4095.0) * 6.6; // Scale the ADC value then convert to voltage
 
+	// Scale the ADC value then convert to voltage
+	float scaled_value = ((float)adc_value / 4095.0) * 6.6;
 	return scaled_value;
 }
